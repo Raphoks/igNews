0e1fc32a7690314981cff84ccedcc2edeb378f08
Raphael Thomaz <raphaeloliveira@pecege.com>
2022-07-30 17:59:47 -0300

    feat: faz integração com a api do stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,25 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import { Container } from '../components/Container';
 import SubscribeButton from '../components/SubscribeButton';
+import { stripe } from '../Services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
+  const priceFormatted = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(product.amount);
+
   return (
     <>
       <Head>Home | ig.news</Head>
@@ -20,10 +34,10 @@ export default function Home() {
             </h1>
             <p>
               Get access to all the publications <br />
-              <span>for $9.90 month</span>
+              <span>for {priceFormatted} month</span>
             </p>
 
-            <SubscribeButton />
+            <SubscribeButton priceId={product.priceId} />
           </section>
 
           <Image
@@ -37,3 +51,20 @@ export default function Home() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const priceKey = 'price_1LRFkEIre6VM8XlKXL2VCuBL';
+
+  const price = await stripe.prices.retrieve(priceKey);
+
+  const product = {
+    priceId: price.id,
+    amount: price.unit_amount! / 100,
+  };
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, //24h
+  };
+};
